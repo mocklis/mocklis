8221aa57062ff60f1e29ab7f57c564489d98f6ed
Esbjörn Redmo <esbjorn.redmo@gmail.com>
2018-12-29 16:28:12 +0000

    Add roslyn analyzer version of Mocklis

---
+++ src/Mocklis.Analyzer/MocklisAnalyzer.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Collections.Immutable;
+using System.Linq;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace Mocklis.Analyzer
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public class MocklisAnalyzer : DiagnosticAnalyzer
+    {
+        public const string DiagnosticId = "MocklisAnalyzer";
+
+        // You can change these strings in the Resources.resx file. If you do not want your analyzer to be localize-able, you can use regular strings for Title and MessageFormat.
+        // See https://github.com/dotnet/roslyn/blob/master/docs/analyzers/Localizing%20Analyzers.md for more on localization
+        private static readonly LocalizableString Title = new LocalizableResourceString(nameof(Resources.AnalyzerTitle), Resources.ResourceManager, typeof(Resources));
+        private static readonly LocalizableString MessageFormat = new LocalizableResourceString(nameof(Resources.AnalyzerMessageFormat), Resources.ResourceManager, typeof(Resources));
+        private static readonly LocalizableString Description = new LocalizableResourceString(nameof(Resources.AnalyzerDescription), Resources.ResourceManager, typeof(Resources));
+        private const string Category = "Code Updating";
+
+        private static DiagnosticDescriptor Rule = new DiagnosticDescriptor(DiagnosticId, Title, MessageFormat, Category, DiagnosticSeverity.Info, isEnabledByDefault: true, description: Description);
+
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get { return ImmutableArray.Create(Rule); } }
+
+        public override void Initialize(AnalysisContext context)
+        {
+            context.RegisterSyntaxNodeAction(AnalyzeNode, SyntaxKind.ClassDeclaration);
+        }
+
+        private void AnalyzeNode(SyntaxNodeAnalysisContext context)
+        {
+            if (context.Node is ClassDeclarationSyntax classDecl && MightBeMocklisClass(classDecl))
+            {
+                var diagnostic = Diagnostic.Create(Rule, context.Node.GetLocation());
+                context.ReportDiagnostic(diagnostic);
+            }
+            throw new NotImplementedException();
+        }
+
+        private bool MightBeMocklisClass(ClassDeclarationSyntax classDecl)
+        {
+            var hasMocklisAttribute = classDecl.AttributeLists.SelectMany(al => al.Attributes).Any(a =>
+                a.Name.DescendantTokens().Any(t => t.Text == "MocklisClass" || t.Text == "MocklisClassAttribute"));
+
+            var isPartial = classDecl.Modifiers.Any(m => m.IsKind(SyntaxKind.PartialKeyword));
+
+            return hasMocklisAttribute && !isPartial;
+        }
+       
+    }
+}
---
+++ src/Mocklis.Analyzer/MocklisCodeFixProvider.cs
@@ -0,0 +1,108 @@
+using System.Collections.Immutable;
+using System.Composition;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CodeFixes;
+using Microsoft.CodeAnalysis.CodeActions;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace Mocklis.Analyzer
+{
+    using Mocklis.CodeGeneration;
+
+    [ExportCodeFixProvider(LanguageNames.CSharp, Name = nameof(MocklisCodeFixProvider)), Shared]
+    public class MocklisCodeFixProvider : CodeFixProvider
+    {
+        private const string MocklisEmptiedClass = "mocklis_emptied_class";
+
+        private const string Title = "Update Mocklis Class (codefix)";
+
+        public sealed override ImmutableArray<string> FixableDiagnosticIds
+        {
+            get { return ImmutableArray.Create(MocklisAnalyzer.DiagnosticId); }
+        }
+
+        public sealed override FixAllProvider GetFixAllProvider() => null;
+        
+        public sealed override async Task RegisterCodeFixesAsync(CodeFixContext context)
+        {
+            var root = await context.Document.GetSyntaxRootAsync(context.CancellationToken).ConfigureAwait(false);
+
+            // TODO: Replace the following code with your own analysis, generating a CodeAction for each fix to suggest
+            var diagnostic = context.Diagnostics.First();
+            var diagnosticSpan = diagnostic.Location.SourceSpan;
+
+            // Find the type declaration identified by the diagnostic.
+            var declaration = root.FindToken(diagnosticSpan.Start).Parent.AncestorsAndSelf().OfType<TypeDeclarationSyntax>().First();
+
+            // Register a code action that will invoke the fix.
+            context.RegisterCodeFix(
+                CodeAction.Create(
+                    title: Title,
+                    createChangedSolution: c => UpdateMocklisClassAsync(context.Document, declaration, c),
+                    equivalenceKey: Title),
+                diagnostic);
+        }
+
+        private async Task<Solution> UpdateMocklisClassAsync(Document document, TypeDeclarationSyntax typeDecl, CancellationToken cancellationToken)
+        {
+            if (!(typeDecl is ClassDeclarationSyntax classDecl))
+            {
+                return document.Project.Solution;
+            }
+
+            var semanticModel = await document.GetSemanticModelAsync(cancellationToken).ConfigureAwait(false);
+            MocklisSymbols mocklisSymbols = new MocklisSymbols(semanticModel.Compilation);
+
+            bool isMocklisClass = classDecl.AttributeLists.SelectMany(al => al.Attributes)
+                .Any(a => semanticModel.GetSymbolInfo(a).Symbol.ContainingType == mocklisSymbols.MocklisClassAttribute);
+
+            if (!isMocklisClass)
+            {
+                return document.Project.Solution;
+            }
+
+            var oldRoot = await document.GetSyntaxRootAsync(cancellationToken).ConfigureAwait(false);
+
+            var emptyClassDecl = MocklisClass.EmptyMocklisClass(classDecl).WithAdditionalAnnotations(new SyntaxAnnotation(MocklisEmptiedClass));
+            var emptyRoot = oldRoot.ReplaceNode(classDecl, emptyClassDecl);
+            var emptyDoc = document.WithSyntaxRoot(emptyRoot);
+            emptyRoot = await emptyDoc.GetSyntaxRootAsync(cancellationToken).ConfigureAwait(false);
+
+            semanticModel = await emptyDoc.GetSemanticModelAsync(cancellationToken).ConfigureAwait(false);
+
+            // And find the class again
+            emptyClassDecl = emptyRoot.DescendantNodesAndSelf().OfType<ClassDeclarationSyntax>()
+                .FirstOrDefault(n => n.GetAnnotations("mocklis_emptied_class").Any());
+
+            if (emptyClassDecl == null)
+            {
+                return document.Project.Solution;
+            }
+
+            var newClassDecl = MocklisClass.UpdateMocklisClass(semanticModel, emptyClassDecl, mocklisSymbols);
+
+            var newRoot = emptyRoot.ReplaceNode(emptyClassDecl, newClassDecl);
+            return emptyDoc.WithSyntaxRoot(newRoot).Project.Solution;
+
+
+
+
+
+            //// Get the symbol representing the type to be renamed.
+            //var semanticModel = await document.GetSemanticModelAsync(cancellationToken);
+            //var typeSymbol = semanticModel.GetDeclaredSymbol(typeDecl, cancellationToken);
+
+            //// Produce a new solution that has all references to that type renamed, including the declaration.
+            //var originalSolution = document.Project.Solution;
+            //var optionSet = originalSolution.Workspace.Options;
+            //var newSolution = await Renamer.RenameSymbolAsync(document.Project.Solution, typeSymbol, newName, optionSet, cancellationToken).ConfigureAwait(false);
+
+            //// Return the new solution with the now-uppercase type name.
+            //return newSolution;
+        }
+    }
+}
