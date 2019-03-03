460ed662224f686d5ea1e736f86f3ddc889f5e6f
Esbjörn Redmo <esbjorn.redmo@gmail.com>
2019-03-03 21:44:51 +0000

    Make gate step expose task directly and handle cancel and error. Code comments

--- src/Mocklis.Experimental/GateStepExtensions.cs
+++ src/Mocklis.Experimental/GateStepExtensions.cs
@@ -9,20 +9,33 @@ namespace Mocklis
     #region Using Directives
 
     using System.Threading;
+    using System.Threading.Tasks;
     using Mocklis.Core;
     using Mocklis.Steps.Gate;
 
     #endregion
 
+    /// <summary>
+    ///     A class with extension methods for adding 'gate' steps to an existing mock or step.
+    /// </summary>
     public static class GateStepExtensions
     {
+        /// <summary>
+        ///     Introduces a step that will complete a task when a method has been called.
+        /// </summary>
+        /// <typeparam name="TParam">The method parameter type.</typeparam>
+        /// <typeparam name="TResult">The method return type.</typeparam>
+        /// <param name="caller">The mock or step to which this 'gate' step is added.</param>
+        /// <param name="task">Returns a reference to the task.</param>
+        /// <param name="cancellationToken">An optional cancellation token.</param>
+        /// <returns>An <see cref="ICanHaveNextMethodStep{TParam, TResult}" /> that can be used to add further steps.</returns>
         public static ICanHaveNextMethodStep<TParam, TResult> Gate<TParam, TResult>(
             this ICanHaveNextMethodStep<TParam, TResult> caller,
-            out IGate gate,
+            out Task<TResult> task,
             CancellationToken cancellationToken = default)
         {
             var newStep = new GateMethodStep<TParam, TResult>(cancellationToken);
-            gate = newStep;
+            task = newStep.Task;
             return caller.SetNextStep(newStep);
         }
     }
--- src/Mocklis.Experimental/Steps/Gate/GateMethodStep.cs
+++ src/Mocklis.Experimental/Steps/Gate/GateMethodStep.cs
@@ -15,23 +15,53 @@ namespace Mocklis.Steps.Gate
 
     #endregion
 
-    public class GateMethodStep<TParam, TResult> : MethodStepWithNext<TParam, TResult>, IGate
+    /// <summary>
+    ///     Class that represents a 'Gate' method step, that will complete a Task the first time the method has been called.
+    ///     Inherits from the <see cref="MethodStepWithNext{TParam,TResult}" /> class.
+    /// </summary>
+    /// <typeparam name="TParam">The method parameter type.</typeparam>
+    /// <typeparam name="TResult">The method return type.</typeparam>
+    /// <seealso cref="MethodStepWithNext{TParam, TResult}" />
+    public class GateMethodStep<TParam, TResult> : MethodStepWithNext<TParam, TResult>
     {
-        private readonly TaskCompletionSource<ValueTuple> _taskCompletionSource;
+        private readonly TaskCompletionSource<TResult> _taskCompletionSource;
 
-        public Task GatePassed => _taskCompletionSource.Task;
+        /// <summary>
+        ///     Gets the task that tracks whether the method has been called..
+        /// </summary>
+        public Task<TResult> Task => _taskCompletionSource.Task;
 
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="GateMethodStep{TParam, TResult}" /> class.
+        /// </summary>
+        /// <param name="cancellationToken">An optional cancellation token that can be used to cancel the task.</param>
         public GateMethodStep(CancellationToken cancellationToken = default)
         {
-            _taskCompletionSource = new TaskCompletionSource<ValueTuple>();
+            _taskCompletionSource = new TaskCompletionSource<TResult>();
             cancellationToken.Register(() => _taskCompletionSource.TrySetCanceled(cancellationToken));
         }
 
+        /// <summary>
+        ///     Called when the mocked method is called.
+        ///     This implementation will complete the task when the method returns, or fail the task if the method throws an
+        ///     exception.
+        /// </summary>
+        /// <param name="mockInfo">Information about the mock through which the method is called.</param>
+        /// <param name="param">The parameters used.</param>
+        /// <returns>The returned result.</returns>
         public override TResult Call(IMockInfo mockInfo, TParam param)
         {
-            var result = base.Call(mockInfo, param);
-            _taskCompletionSource.TrySetResult(default);
-            return result;
+            try
+            {
+                var result = base.Call(mockInfo, param);
+                _taskCompletionSource.TrySetResult(default);
+                return result;
+            }
+            catch (Exception e)
+            {
+                _taskCompletionSource.TrySetException(e);
+                throw;
+            }
         }
     }
 }
